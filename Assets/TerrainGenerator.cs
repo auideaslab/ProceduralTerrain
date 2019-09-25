7da72740f65799780e03b889f3aafaef709d609d
auideaslab <krzysztofvr@gmail.com>
2019-09-25 18:42:48 -0400

    Procedural Terrain

---
+++ Assets/TerrainGenerator.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainGenerator : MonoBehaviour
+{
+    Terrain terrain;
+    // Start is called before the first frame update
+    void Start()
+    {
+        terrain = GetComponent<Terrain>();
+        float[,] heightmap = terrain.terrainData.GetHeights(0,0, terrain.terrainData.heightmapWidth, terrain.terrainData.heightmapHeight);   
+
+        for (int i=0; i < terrain.terrainData.heightmapHeight; i++)
+        {
+            for (int j=0; j <terrain.terrainData.heightmapWidth; j++)
+            {
+                float x = j / (float) terrain.terrainData.heightmapWidth;
+                float y = i / (float) terrain.terrainData.heightmapHeight;
+                float height = Mathf.PerlinNoise(x, y);
+                heightmap[i, j] = height;
+            }
+        }
+        terrain.terrainData.SetHeights(0, 0, heightmap);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
